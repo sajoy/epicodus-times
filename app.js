171d6f13ca662ffc00241aa40591bf0a714a18ea
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-27 11:07:12 -0800

    add listing functionality, form to add new reporter

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ epicodusTimes.config(function($stateProvider) {
 
   $stateProvider.state('home', {
     url: "",
-    templateUrl: "partials/home.html",
-    controller: "DivisionsController"
+    templateUrl: "partials/home.html"
   });
 
   $stateProvider.state('home.divisions', {
     url: "/divisions",
-    templateUrl: "partials/home.divisions.html"
+    templateUrl: "partials/home.divisions.html",
+    controller: "DivisionsController"
   });
 
 });
