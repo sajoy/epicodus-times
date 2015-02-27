72f2de83de2a8783585c0529af6e6b017af7be23
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-27 13:43:33 -0800

    change layout

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,9 +1,10 @@
-epicodusTimes.controller('ReportersController', function ReportersCtrl($scope, $stateParams, DivisionsFactory, UtilitiesFactory) {
+epicodusTimes.controller('ReportersController', function ReportersCtrl($scope, $state, $stateParams, DivisionsFactory, UtilitiesFactory) {
   $scope.division = UtilitiesFactory.findById(DivisionsFactory.divisions, $stateParams.divisionId)
   $scope.addReporter = function() {
     $scope.division.reporters.push({name: $scope.reporterName, years: $scope.years, birthday: $scope.birthday})
     $scope.reporterName = null;
     $scope.years = null;
     $scope.birthday = null;
+    $state.go('home.divisions');
   }
 });
