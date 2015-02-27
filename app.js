28f197ce70a53d00aaee45ed360d1aa4cbaaa3a2
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-27 09:48:17 -0800

    add partials, begin Division Controller, Factory

---
+++ app.js
@@ -0,0 +1,10 @@
+var epicodusTimes  = angular.module('epicodusTimes', ['ui.router']);
+
+epicodusTimes.config(function($stateProvider) {
+
+  $stateProvider.state('home', {
+    url: "";
+    templateUrl: "partials/home.html"
+  });
+
+});
